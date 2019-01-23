05396aed6942a5ecbcfa00098a114057a0dc73c7
Thomas Steiner <steiner.thomas@gmail.com>
2019-01-23 13:45:01 +0000

    Flatten folder structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,17 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+//const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(pluginRss);
+  //eleventyConfig.addPlugin(pluginSyntaxHighlight);
+  eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.addPassthroughCopy('static');
+  eleventyConfig.addPassthroughCopy('images');
+
+  return {
+    dir: {
+      output: "dist"
+    }
+  };
 };
