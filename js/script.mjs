aa7082a649dab7add32e659b1680ea0dd96c9772
tomayac <steiner.thomas@gmail.com>
2019-11-29 11:26:28 +0100

    Meta theme color

--- js/script.mjs
+++ js/script.mjs
@@ -1,2 +1,10 @@
 import './dark-mode-toggle.min.mjs';
 
+const darkModeToggle = document.querySelector('dark-mode-toggle');
+const themeColor = document.querySelector('meta[name="theme-color"]');
+const root = document.documentElement;
+
+darkModeToggle.addEventListener('colorschemechange', () => {
+  themeColor.content = getComputedStyle(root).getPropertyValue('--background-color');
+});
+themeColor.content = getComputedStyle(root).getPropertyValue('--background-color');
