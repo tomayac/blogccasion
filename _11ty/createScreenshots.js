6ae25e69c548d90c5f40ab79f68e8fbf5f58434d
tomayac <steiner.thomas@gmail.com>
2019-12-02 17:21:39 +0100

    Add image fallback

---
+++ _11ty/createScreenshots.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const fs = require('fs').promises;
+const xml2js = require('xml2js');
+const puppeteer = require('puppeteer');
+
+const parseSitemap = async () => {
+  const sitemap = await fs.readFile('_site/sitemap.xml', {encoding: 'utf-8'});
+  try {
+    const xml = await xml2js.parseStringPromise(sitemap);
+    const urls = [];
+    if (xml.urlset && xml.urlset.url) {
+      xml.urlset.url.forEach((url, i) => {
+        urls[i] = url.loc[0];
+      });
+    }
+    return urls;
+  } catch (err) {
+    console.error(err.name, err.message);
+  }
+};
+
+const createScreenshots = async (page, url, output) => {
+  try {
+    console.log('Creating screenshots for', url);
+    await page.goto(url);
+    // Create Light Mode screenshot
+    await page.emulateMediaFeatures([{
+      name: 'prefers-color-scheme', value: 'light'}]);
+    await page.screenshot({path: `${output}-light.png`});
+    // Create Dark Mode screenshot
+    await page.emulateMediaFeatures([{
+      name: 'prefers-color-scheme', value: 'dark' }]);
+    await page.screenshot({path: `${output}-dark.png`});
+  } catch (err) {
+    console.error(err.name, err.message);
+  }
+};
+
+const start = async () => {
+  const browser = await puppeteer.launch({
+    // The macOS firewall might complain, see
+    // https://github.com/GoogleChrome/puppeteer/issues/4752.
+    headless: true,
+  });
+  const page = await browser.newPage();
+
+  const urls = await parseSitemap();
+  for await (const url of urls) {
+    const output =
+        `./_site/${url.replace('https://blog.tomayac.com/', '')}screenshot`;
+    await createScreenshots(page, url, output);
+  }
+  await browser.close();
+};
+
+start();
--- js/script.mjs
+++ js/script.mjs
@@ -8,3 +8,40 @@ darkModeToggle.addEventListener('colorschemechange', () => {
   themeColor.content = getComputedStyle(root).getPropertyValue('--background-color');
 });
 themeColor.content = getComputedStyle(root).getPropertyValue('--background-color');
+
+const main = document.querySelector('main');
+const imgs = main.querySelectorAll('img');
+
+const fallback = '/static/fallback.svg';
+
+const timeouts = {};
+
+const onError = e => {
+  const img = e.target;
+  img.src = fallback;
+};
+
+const onLoad = e => {
+  const img = e.target;
+  clearTimeout(timeouts[img.src]);
+  delete timeouts[img.src];
+  img.removeEventListener("error", onError);
+};
+
+imgs.forEach(img => {
+  img.addEventListener("error", onError, { once: true });
+  img.addEventListener("load", onLoad, { once: true });
+
+  timeouts[img.src] = setTimeout(() => {
+    img.removeEventListener("error", onError);
+    img.removeEventListener("load", onLoad);
+    img.src = fallback;
+  }, 10 * 1000);
+
+  if (img.complete) {
+    img.removeEventListener("load", onLoad);
+    img.removeEventListener("error", onError);
+    clearTimeout(timeouts[img.src]);
+    delete timeouts[img.src];
+  }
+});
