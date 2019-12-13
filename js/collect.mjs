c41aee81f68703cf9f719afb2b972254decf057b
tomayac <steiner.thomas@gmail.com>
2019-12-13 09:48:39 +0100

    Add collection script

---
+++ js/collect.mjs
@@ -0,0 +1,24 @@
+(async (r, l, s, d, h, e, u, n, i, c) => {
+  const o = {
+    z: `${r()}`.substr(2),
+    cid: `${l.getItem(i) || (c = `${r()}`.substr(2), l.setItem(i, c), c)}`,
+    ua: n.userAgent,
+    dr: d.referrer || '',
+    sr: `${s.width}x${s.height}`,
+    vp: `${h.clientWidth}x${h.clientHeight}`,
+    sd: `${s.pixelDepth}-bits`,
+    ul: n.language,
+    dl: e(u.href),
+    dp: e(u.pathname),
+    dt: d.title
+  };
+  const f = new FormData();
+  for (let [k, v] of Object.entries(o)) {
+    f.append(k, v);
+  }
+  fetch('https://tomayac.com/cors-proxy/collect.php', {
+    method: 'post',
+    body: f
+  });
+})(Math.random, localStorage, screen, document, document.documentElement,
+    encodeURIComponent, location, navigator, 'cid', 0);
