1254a2b06b97762d04bbaeca9d4506a129f5c0f2
tomayac <steiner.thomas@gmail.com>
2021-02-11 15:11:33 +0100

    Make sure copy code only runs once

--- js/script.mjs
+++ js/script.mjs
@@ -30,22 +30,28 @@ const LOAD = 'load';
 if (articleBody) {
   if ('clipboard' in navigator) {
     articleBody
-      // ToDo: Use `:is(h1, h2, h3, h4, h5, h6)[id]` once support is better.
-      .querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]')
+      // ToDo: Use `:is(h2, h3, h4, h5, h6)[id]` once support is better.
+      .querySelectorAll('h2[id], h3[id], h4[id], h5[id], h6[id]')
       .forEach((heading) => {
         heading.addEventListener('click', (ev) => {
           // Don't jump when the '#' is clicked.
           if (ev.target.nodeName === 'A') {
             ev.preventDefault();
           }
+          // Only run once.
+          if (heading.dataset.toasted) {
+            return;
+          }
           try {
             navigator.clipboard.writeText(
               `${location.origin}${location.pathname}#${heading.id}`,
             );
             const temp = heading.innerHTML;
-            heading.innerHTML += '&nbsp;<small>(📋 Copied.)</small>';
+            heading.innerHTML += '&nbsp;<small>(📋 Copied)</small>';
+            heading.dataset.toasted = 'toasted';
             setTimeout(() => {
               heading.innerHTML = temp;
+              delete heading.dataset.toasted;
             }, 2000);
           } catch (err) {
             console.warn(err.name, err.message);
@@ -53,7 +59,7 @@ if (articleBody) {
         });
       });
   }
-   
+
   imgs = articleBody.querySelectorAll('img');
 
   const timeouts = {};
