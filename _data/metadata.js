9724535e1d0d16f4e3611a67ef62fa7fc7098e7a
Thomas Steiner <tomac@google.com>
2024-12-16 18:59:30 +0100

    Add cache

---
+++ _data/metadata.js
@@ -0,0 +1,18 @@
+export default {
+  title: 'Blogccasion',
+  domain: 'blog.tomayac.com',
+  webmention_io_token: 'SWdMjuLekI1OMYPYpf497w',
+  url: 'https://blog.tomayac.com',
+  description: 'The personal blog of Thomas Steiner',
+  feed: {
+    subtitle: 'A look inside the think tank',
+    filename: 'feed.xml',
+    path: '/feed/feed.xml',
+    url: 'https://blog.tomayac.com/feed/feed.xml',
+    id: 'https://blog.tomayac.com/',
+  },
+  author: {
+    name: 'Thomas Steiner',
+    email: 'steiner.thomas@gmail.com',
+  },
+};
